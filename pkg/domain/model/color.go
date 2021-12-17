51e05fc52b3f71cc07d5364892342bd7ac04a31d
THEToilet <bp18015@shibaura-it.ac.jp>
2021-12-17 23:26:42 +0900

    :sparkles: Color の定数を定義

---
+++ pkg/domain/model/color.go
@@ -0,0 +1,15 @@
+package pkg.domain.model
+
+
+type Color string
+
+const (
+	Gray = Color("#008080")
+	Brown = Color("#004000")
+	Green = Color("#008000")
+	LightBlue = Color("#00C0C0")
+	Blue = Color("#0000FF")
+	Yellow = Color("#C0C000")
+	Orange = Color("#FF8000")
+	Red = Color("#FF0000")
+)
