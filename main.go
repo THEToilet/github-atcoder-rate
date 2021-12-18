901817415a5683cc44b5c201c7f2b84b5a701e70
THEToilet <bp18015@shibaura-it.ac.jp>
2021-12-19 02:51:36 +0900

    :sparkles: add server.go

--- main.go
+++ main.go
@@ -1,65 +1,14 @@
 package main
 
 import (
-	"github-program-rate/pkg/domain/model"
-	svg "github.com/ajstarks/svgo"
+	"github-program-rate/pkg/gateway"
 	"log"
-	"net/http"
 )
 
 func main() {
-	http.Handle("/svg", http.HandlerFunc(circle))
-	err := http.ListenAndServe(":82", nil)
-	if err != nil {
+	server := gateway.NewServer(logger)
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 
-}
-
-func circle(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "image/svg+xml")
-	canvas := svg.New(w)
-	/*
-		s.Start(800, 800)
-		s.Circle(300, 300, 125, "fill:none;stroke:#3ab60b")
-		s.Circle(305, 300, 105, "fill:none;stroke:#3ab60b")
-		s.End()
-	*/
-
-	width := 500
-	height := 500
-	angle, cx, cy := 30.0, width/2, height/2
-	r := width / 4
-
-	canvas.Start(width, height)
-
-	// NOTE: 背景
-	canvas.Gstyle("fill:white")
-	canvas.Rect(0, 0, width, height)
-	canvas.Gend()
-
-	// NOTE: 外枠
-	canvas.Gstyle("fill:none;stroke:black")
-	canvas.Roundrect(0, 0, width, height, 10, 10)
-	canvas.Gend()
-
-	canvas.Gstyle(string("fill:" + model.Gray))
-	canvas.TranslateRotate(cx, cy, -angle)
-	canvas.Arc(-r, 0, r, r, 30, false, true, r, 0)
-	canvas.Gend()
-
-	canvas.TranslateRotate(cx, cy, angle)
-	canvas.Arc(-r, 0, r, r, 30, false, false, r, 0)
-	canvas.Gend()
-
-	canvas.Gstyle("fill:white")
-	canvas.Circle(width/2, height/2, 90)
-	canvas.Gend()
-
-	canvas.Gstyle("fill:black")
-	canvas.Text(width/2, height/2, "730")
-	canvas.Gend()
-
-	canvas.Gend()
-	canvas.End()
 }
